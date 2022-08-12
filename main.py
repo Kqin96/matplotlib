63b3f295a7a8a1874dfd7f475ae122f58f84d255
Kqin <97442619+Kqin96@users.noreply.github.com>
2022-08-12 16:52:17 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+a=input('Aの値')
+A=int(a)
+b=input('Bの値')
+B=int(b)
+c=input('Cの値')
+C=int(c)
+d=input('Dの値')
+D=int(d)
+e=input('Eの値')
+E=int(e)
+data_temps = [a, b, c, d, e]
+plt.plot(data_temps)
+plt.show()
